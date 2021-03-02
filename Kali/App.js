aba04a1b738b9164d531c18ec6bcbb1ca05ddc43
Theodore Bendixson <tedbendixson@me.com>
2021-03-01 19:21:18 -0700

    Full scrolling one page screen iOS

--- Kali/App.js
+++ Kali/App.js
@@ -22,53 +22,53 @@ const App: () => React$Node = () => {
                     { backgroundColor: KaliColors.creamBackground } 
                 }
       >
-        <Text style = { 
-                        { paddingTop: 20, paddingLeft: 20, paddingRight: 20, lineHeight: 46,
-                          textAlign: "center", fontFamily: KaliFonts.pusab, fontSize: 64, color: KaliColors.textTurquoise } 
-                      }>
-            KALI THE CODER
-        </Text>
-        <View style = { { height: 20 } }/>
-        <Text style = { 
-                        { textAlign: "center", fontFamily: KaliFonts.pusab, fontSize: 24, color: '#ff8f0d',
-                          lineHeight: 24} 
-                      }>
-            LEARN THE ABC's 
-        </Text>
-        <View style = { { height: 20 } }/>
-        <Image source = {require('./assets/images/group-100.png')}
-               style = { { alignSelf: 'center', width: 240, height: 280, resizeMode: 'contain' } } 
-        />
-        <View style = { { height: 20 } }/>
-        <View style = { { backgroundColor: '#FFFFFF', width: 300, height: 100, alignSelf: 'center', borderRadius: 15  } } >
-            <Text style = { { textAlign: 'center', fontFamily: KaliFonts.pusab, fontSize: 24, color: KaliColors.textTurquoise,
-                              lineHeight: 24, paddingTop: 10 } } >
-                USING THE APP
+        <ScrollView>
+            <Text style = { 
+                            { paddingTop: 20, paddingLeft: 20, paddingRight: 20, lineHeight: 46,
+                              textAlign: "center", fontFamily: KaliFonts.pusab, fontSize: 64, color: KaliColors.textTurquoise } 
+                          }>
+                KALI THE CODER
             </Text>
-            <Text style = { {  textAlign: 'center', fontFamily: 'FreightSansProMedium-Regular', fontSize: 14, 
-                               color: KaliColors.textTurquoise, lineHeight: 16, paddingTop: 10 } }>
-                Be sure to enable sound on your Apple Watch.
-                Twisting the digital crown will rotate Kali for
-                other viewing options.
+            <View style = { { height: 20 } }/>
+            <Text style = { 
+                            { textAlign: "center", fontFamily: KaliFonts.pusab, fontSize: 24, color: '#ff8f0d',
+                              lineHeight: 24} 
+                          }>
+                LEARN THE ABC's 
             </Text>
-        </View>
-        <View style = { { height: 20 } }/>
-        <Text style = { 
-                        { textAlign: "center", fontFamily: KaliFonts.pusab, fontSize: 24, color: '#ff8f0d',
-                          lineHeight: 24} 
-                      }>
-            THANK YOU FOR LEARNING WITH US! 
-        </Text>
-        <View style = { { height: 20 } }/>
-        <Text style = { {  textAlign: 'center', fontFamily: 'FreightSansProMedium-Regular', fontSize: 20, 
-                           color: KaliColors.textTurquoise, lineHeight: 20, } }>
-            Questions or Support:
-        </Text>
-        <Text style = { {  textAlign: 'center', fontFamily: 'FreightSansProMedium-Regular', fontSize: 20, 
-                           color: KaliColors.textTurquoise, lineHeight: 20, } }>
-            email@kalithecoder.com | kalithecoder.com
-        </Text>
-
+            <View style = { { height: 20 } }/>
+            <Image source = {require('./assets/images/group-100.png')}
+                   style = { { alignSelf: 'center', width: 240, height: 280, resizeMode: 'contain' } } 
+            />
+            <View style = { { height: 20 } }/>
+            <View style = { { backgroundColor: '#FFFFFF', width: 300, height: 100, alignSelf: 'center', borderRadius: 15  } } >
+                <Text style = { { textAlign: 'center', fontFamily: KaliFonts.pusab, fontSize: 24, color: KaliColors.textTurquoise,
+                                  lineHeight: 24, paddingTop: 10 } } >
+                    USING THE APP
+                </Text>
+                <Text style = { {  textAlign: 'center', fontFamily: 'FreightSansProMedium-Regular', fontSize: 14, 
+                                   color: KaliColors.textTurquoise, lineHeight: 16, paddingTop: 10 } }>
+                    Be sure to enable sound on your Apple Watch.
+                    Twisting the digital crown will rotate Kali for
+                    other viewing options.
+                </Text>
+            </View>
+            <View style = { { height: 20 } }/>
+            <Text style = { 
+                            { textAlign: "center", fontFamily: KaliFonts.pusab, fontSize: 24, color: '#ff8f0d',
+                              lineHeight: 24} 
+                          }>
+                THANK YOU FOR LEARNING WITH US! 
+            </Text>
+            <View style = { { height: 20 } }/>
+            <Text style = { styles.footerText }>
+                Questions or Support:
+            </Text>
+            <Text style = { styles.footerText }>
+                email@kalithecoder.com | kalithecoder.com
+            </Text>
+            <View style = { { height: 20 } }/>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -84,7 +84,9 @@ const KaliColors = {
 }
 
 const styles = StyleSheet.create({
-    
+    footerText: {  textAlign: 'center', fontFamily: 'FreightSansProMedium-Regular', fontSize: 16, 
+                   color: KaliColors.textTurquoise, lineHeight: 22
+    }
 });
 
 export default App;
