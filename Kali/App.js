9158d4fede8d7bf9b232ae8fed8dd57a37987337
Theodore Bendixson <tedbendixson@me.com>
2021-03-01 17:33:16 -0700

    Amazingly, guessing at how to layout text in React Native works because everything in web development is totally arbitrary so now we're programming by consulting the magical oracle

--- Kali/App.js
+++ Kali/App.js
@@ -20,57 +20,26 @@ const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Welcome to Kali The Coder!</Text>
-              <Text style={styles.sectionDescription}>
-               This is the iPhone companion app. Open the app on your Apple Watch to interact with Kali. Build Number 34.
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
+      <SafeAreaView
+        style = { 
+                    { flex: 1, flexDirection: "row", backgroundColor: KaliColors.creamBackground } 
+                }
+      >
+        <Text style = { 
+                        { marginTop: 10, fontFamily: 'Pusab', fontSize: 32, color: '#24b4c1' } 
+                      }>
+        Kali The Coder
+        </Text>
       </SafeAreaView>
     </>
   );
 };
 
+const KaliColors = {
+    creamBackground: '#FFF7E7'
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
 });
 
 export default App;
